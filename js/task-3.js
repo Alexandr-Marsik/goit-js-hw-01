0d87062cc649b127e71a7c1186030dcd17e22dec
Alexandr <lesiv.sania@gmail.com>
2025-10-26 02:30:41 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -3,13 +3,10 @@ function getElementWidth(content, padding, border) {
   const contentWidth = Number.parseFloat(content);
   const paddingWidth = Number.parseFloat(padding);
   const borderWidth = Number.parseFloat(border);
-
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+  const horizontalSpace = (paddingWidth + borderWidth) * 2;
+  return contentWidth + horizontalSpace;
 }
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
